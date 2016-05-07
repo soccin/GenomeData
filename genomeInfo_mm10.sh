bb6e294c4561f930962cc8079c04c624865fef03
Nicholas D. Socci <soccin@mskcc.org>
2016-05-07 13:43:50 -0400

    Added FACETSNPS paths

--- genomeInfo_mm10.sh
+++ genomeInfo_mm10.sh
@@ -3,3 +3,4 @@
 GENOME=/ifs/work/socci/Depot/Genomes/M.musculus/ucsc/mm10/mm10_Fixed.fasta
 GENOMEFAI=/ifs/work/socci/Depot/Genomes/M.musculus/ucsc/mm10/mm10_Fixed.fasta.fai
 EXACDB=""
+FACETSNPS="/home/socci/Code/Pipelines/FACETS/FACETS.app/lib/mouse_MM10_dbSNP_NCBI_20150625___noChrM__plusPseudo50__DROP_SORT.vcf.gz"
